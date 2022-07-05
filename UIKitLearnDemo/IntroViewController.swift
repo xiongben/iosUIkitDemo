af0ea2f0cfa11d8b7531444aef225d2357af287c
kakaxi <kakaxi@gmail.com>
2022-07-05 23:19:35 +0800

    [add]

---
+++ UIKitLearnDemo/IntroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IntroViewController.swift
+//  UIKitLearnDemo
+//
+//  Created by admin on 2022/7/5.
+//
+
+import UIKit
+
+class IntroViewController : UIViewController {
+    
+    let fullSize = UIScreen.main.bounds.size
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.white
+        
+        // 頁面標題
+        let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: fullSize.width, height: 40))
+        myLabel.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.08)
+        myLabel.textAlignment = .center
+        myLabel.text = "Intro 頁"
+        self.view.addSubview(myLabel)
+    }
+    
+    override func viewWillLayoutSubviews() {
+            super.viewWillLayoutSubviews()
+    }
+}
--- UIKitLearnDemo/ViewController.swift
+++ UIKitLearnDemo/ViewController.swift
@@ -9,46 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let label = UILabel();
+    // 取得螢幕的尺寸
+    let fullSize = UIScreen.main.bounds.size
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // 加入父控件
-        view.addSubview(label)
-        // 背景颜色
-        label.backgroundColor = UIColor.cyan;
-        // 设置内容
-        label.text = "hello world!!! this is a test"
-        // 设置文字颜色
-        label.textColor = UIColor.red
-        // 设置字体样式和大小
-        label.font = UIFont(name:"Zapfino", size:15)
-        // 设置字体大小
-        //label.font = UIFont.systemFont(ofSize:20)
-        // 设置字体居中
-        label.textAlignment = .center
-        // 文本多行显示
-        label.numberOfLines = 0
-        // 设置阴影
-        label.shadowColor = UIColor.gray
-        label.shadowOffset = CGSize(width:1.5, height:1.5)
-        // 文字过长时的省略方式
-        label.lineBreakMode = .byTruncatingTail // 隐藏尾部并显示省略号
-        label.lineBreakMode = .byTruncatingMiddle // 隐藏中间部分并显示省略号
-        label.lineBreakMode = .byTruncatingHead // 隐藏头部并显示省略号
-        label.lineBreakMode = .byClipping // 截去多余部分也不显示省略号
-        // 设置tag
-        label.tag = 1003
-        // 获取方法
-//        let getTagView = view.viewWithTag(1003) as! UILabel
+        
+        // 建立前往 Intro 頁面的 UIButton
+        let myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        myButton.setTitle("Intro", for: .normal)
+        myButton.backgroundColor = UIColor.lightGray
+        myButton.addTarget(self, action: #selector(goIntro), for: .touchDown)
+        myButton.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.4)
+        self.view.addSubview(myButton)
     }
 
     
     override func viewWillLayoutSubviews() {
             super.viewWillLayoutSubviews()
-            // 设置位置尺寸
-            label.frame = CGRect(x:20, y:100, width:200, height:40)
-        }
+    }
+    
+    @objc func goIntro() {
+        
+        let introViewController = IntroViewController()
+        print("viewWillDisappear")
+        self.present(introViewController, animated: true, completion: nil)
+    }
 }
 
