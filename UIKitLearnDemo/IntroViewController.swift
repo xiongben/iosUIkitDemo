232a957d3b987144406c08549d17e2357e64e8ec
kakaxi <kakaxi@gmail.com>
2022-07-06 17:51:39 +0800

    [add]

--- UIKitLearnDemo/IntroViewController.swift
+++ UIKitLearnDemo/IntroViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import WebKit
 
-class IntroViewController : UIViewController {
+class IntroViewController : UIViewController, UITextFieldDelegate, WKNavigationDelegate  {
     
     let fullSize = UIScreen.main.bounds.size
+    var myWebView :WKWebView!
+    var myActivityIndicator:UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,24 +26,35 @@ class IntroViewController : UIViewController {
         myLabel.text = "Intro 頁"
         self.view.addSubview(myLabel)
         
-        let myImageView = UIImageView(
-          frame: CGRect(
-            x: 0, y: 0, width: 200, height: 200))
-        let imgArr = [
-            UIImage(named: "01.png")!,
-            UIImage(named: "02.png")!,
-            UIImage(named: "03.png")!,
-        ]
-        myImageView.animationImages = imgArr
-        myImageView.animationDuration = 6
-        myImageView.animationRepeatCount = 0
-        myImageView.startAnimating()
+        myWebView = WKWebView(frame: CGRect(x: 0, y: 30, width: fullSize.width,height: fullSize.height - 30))
+        myWebView.navigationDelegate = self
+        self.view.addSubview(myWebView)
+        let myURL = URL(string:"https://www.baidu.com")
+        let myRequest = URLRequest(url: myURL!)
+        myWebView.load(myRequest)
+
         
-        myImageView.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.4)
-        self.view.addSubview(myImageView)
+        
+        
+//        let myImageView = UIImageView(
+//          frame: CGRect(
+//            x: 0, y: 0, width: 200, height: 200))
+//        let imgArr = [
+//            UIImage(named: "01.png")!,
+//            UIImage(named: "02.png")!,
+//            UIImage(named: "03.png")!,
+//        ]
+//        myImageView.animationImages = imgArr
+//        myImageView.animationDuration = 6
+//        myImageView.animationRepeatCount = 0
+//        myImageView.startAnimating()
+//
+//        myImageView.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.4)
+//        self.view.addSubview(myImageView)
     }
     
     override func viewWillLayoutSubviews() {
             super.viewWillLayoutSubviews()
     }
+
 }
