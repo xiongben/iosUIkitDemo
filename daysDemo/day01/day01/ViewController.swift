8164d8e47091794653f68157b64cf9f0ef59c41a
kakaxi <kakaxi@gmail.com>
2022-07-24 15:10:27 +0800

    [add] init day01 demo

---
+++ daysDemo/day01/day01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day01
+//
+//  Created by admin on 2022/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ daysDemo/day01/day01UITests/day01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  day01UITestsLaunchTests.swift
+//  day01UITests
+//
+//  Created by admin on 2022/7/24.
+//
+
+import XCTest
+
+class day01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
