ba1bf926fdbba3cf6ad186676f7289b3c4832b7c
kakaxi <kakaxi@gmail.com>
2022-07-28 22:34:28 +0800

    [add]

---
+++ daysDemo/day04_LimitInputTextField/LimitInputTextField/LimitInputTextField/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LimitInputTextField
+//
+//  Created by admin on 2022/7/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    var limitedTextView:UITextView!
+    var allowInputNumberLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        
+    }
+    
+    func initNavigationBar() {
+        
+    }
+
+
+}
+
