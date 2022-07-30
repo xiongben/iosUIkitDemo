efde0b8bbf703e53d05f0bec60b43aef832dde01
kakaxi <kakaxi@gmail.com>
2022-07-30 22:43:50 +0800

    [add]

---
+++ daysDemo/day07_SimplePhotoBrowser/SimplePhotoBrowser/SimplePhotoBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimplePhotoBrowser
+//
+//  Created by admin on 2022/7/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
