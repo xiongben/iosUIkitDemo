06efb5a3c29c942dc1e9257079519479b833cefe
kakaxi <kakaxi@gmail.com>
2022-08-12 23:48:23 +0800

    [add]

---
+++ daysDemo/day17/SideNavigationDemo/SideNavigationDemo/SideNavigation-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SideNavigation-Bridging-Header.h
+//  SideNavigationDemo
+//
+//  Created by admin on 2022/8/12.
+//
+
+#ifndef SideNavigation_Bridging_Header_h
+#define SideNavigation_Bridging_Header_h
+
+#import "SWRevealViewController.h"
+
+#endif /* SideNavigation_Bridging_Header_h */
--- daysDemo/day17/SideNavigationDemo/SideNavigationDemo/ThirdViewController.swift
+++ daysDemo/day17/SideNavigationDemo/SideNavigationDemo/ThirdViewController.swift
@@ -1,33 +1,28 @@
 //
-//  ViewController.swift
+//  ThirdViewController.swift
 //  SideNavigationDemo
 //
-//  Created by admin on 2022/8/10.
+//  Created by admin on 2022/8/12.
 //
+
 import UIKit
 
-class ViewController: UIViewController {
+class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let imageView = UIImageView(image: UIImage(named: "ViewController"))
+        let imageView = UIImageView(image: UIImage(named: "ThirdViewController"))
         imageView.frame = self.view.frame
         self.view.addSubview(imageView)
         self.navigationController?.isNavigationBarHidden = true
-        print(self.revealViewController() as Any)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
 }
-
-
-
