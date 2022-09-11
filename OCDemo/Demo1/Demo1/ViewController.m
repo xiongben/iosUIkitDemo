6b518e14fc4fc92e3b82c2c30340ad0f1ab05b5c
kakaxi <kakaxi@gmail.com>
2022-09-11 17:15:29 +0800

    [add]

--- OCDemo/Demo1/Demo1/ViewController.m
+++ OCDemo/Demo1/Demo1/ViewController.m
@@ -24,25 +24,35 @@
 //    label.backgroundColor = [UIColor yellowColor];
 //    [self.view addSubview:label];
     
-    UIImageView *img1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"phoneBg.jpeg"]];
+//    UIImageView *img1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"phoneBg.jpeg"]];
+//
+//    img1.backgroundColor = [UIColor yellowColor];
+////    设置尺寸
+////    img1.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//
+//    img1.frame = self.view.bounds;
+//
+//    img1.contentMode = UIViewContentModeScaleAspectFill;
+//
+////    毛玻璃效果
+//    UIToolbar *toolbar = [[UIToolbar alloc] init];
+//    toolbar.frame = img1.bounds;
+//    toolbar.alpha = 0.9;
+//    toolbar.barStyle = UIBarStyleBlack;
+//    [img1 addSubview:toolbar];
+//
+//
+//    [self.view addSubview:img1];
     
-    img1.backgroundColor = [UIColor yellowColor];
-//    设置尺寸
-//    img1.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//    路径加载图片
+    UIImageView *img2 = [[UIImageView alloc] init];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"thumb" ofType:@"jpeg"];
+    img2.image = [UIImage imageWithContentsOfFile:path];
+    img2.frame = self.view.bounds;
+    [self.view addSubview:img2];
+
+     
     
-    img1.frame = self.view.bounds;
-    
-    img1.contentMode = UIViewContentModeScaleAspectFill;
-    
-//    毛玻璃效果
-    UIToolbar *toolbar = [[UIToolbar alloc] init];
-    toolbar.frame = img1.bounds;
-    toolbar.alpha = 0.9;
-    toolbar.barStyle = UIBarStyleBlack;
-    [img1 addSubview:toolbar];
-    
-    
-    [self.view addSubview:img1];
 }
 
 
