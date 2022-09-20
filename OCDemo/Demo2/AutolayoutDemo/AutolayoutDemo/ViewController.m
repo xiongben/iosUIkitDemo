b2ded4f990af1a7b7c53113d5e7c839bb19363f5
kakaxi <kakaxi@gmail.com>
2022-09-20 17:39:14 +0800

    [ADD]

--- OCDemo/Demo2/AutolayoutDemo/AutolayoutDemo/ViewController.m
+++ OCDemo/Demo2/AutolayoutDemo/AutolayoutDemo/ViewController.m
@@ -6,9 +6,12 @@
 //
 
 #import "ViewController.h"
+#define MAS_SHORTHAND
+#import "Masonry/Masonry.h"
+
+
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -17,11 +20,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIView *redView = [[UIView alloc] init];
+    redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:redView];
+    
+    UIView *blueView = [[UIView alloc] init];
+    blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:blueView];
+    
+//    添加约束
+    [redView makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.top).multipliedBy(1.0).offset(10);
+        make.left.equalTo(self.view.left).offset(30);
+        make.right.equalTo(self.view.right).offset(-30);
+        make.bottom.equalTo(self.view.bottom).offset(-20);
+    }];
+    
+    [blueView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.width.mas_equalTo(100);
+//        make.height.mas_equalTo(100);
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.centerY.mas_equalTo(self.view.mas_centerY);
+    }];
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    self.label.text = @"eeee sgsfd sfgffgd sdgs sdgs fdgf bgs";
-}
 
 
 @end
