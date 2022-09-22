f0936b7f9aea0082104573be9c4bc38bd7b4c176
kakaxi <kakaxi@gmail.com>
2022-09-22 22:56:05 +0800

    [add]

--- OCDemo/Demo1/Demo1/ViewController.m
+++ OCDemo/Demo1/Demo1/ViewController.m
@@ -65,6 +65,21 @@
     redView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
     [self.view addSubview:redView];
     
+////    UILabel高度自定义
+//    UILabel *label1 = [[UILabel alloc] init];
+//    NSString *desc = @"This method is a convenience constructor for creating button objects with specific configurations. If you subclass UIButton, this method does not return an instance of your subclass. If you want to create an instance of a specific subclass, you must alloc/init the button directly.";
+//    label1.text = desc;
+//    label1.font = [UIFont systemFontOfSize:14];
+//    label1.textColor = [UIColor redColor];
+//    label1.backgroundColor = [UIColor yellowColor];
+//    label1.numberOfLines = 0;
+//    CGFloat textW = 150;
+//    NSDictionary *textAtt = @{NSFontAttributeName: [UIFont systemFontOfSize:14]};
+//    CGSize textSize = CGSizeMake(textW, MAXFLOAT);
+//    CGFloat textH = [desc boundingRectWithSize:textSize options:NSStringDrawingUsesLineFragmentOrigin attributes:textAtt context:nil].size.height;
+//    label1.frame = CGRectMake(10, 30, textW, textH);
+//    [self.view addSubview:label1];
+    
     
 //    UITextField *field1 = [[UITextField alloc] initWithFrame:CGRectMake(0, 550, 150, 60)];
 //    field1.backgroundColor = [UIColor yellowColor];
--- OCDemo/TableViewDemo/TableViewDemo/TableViewControllerPage.m
+++ OCDemo/TableViewDemo/TableViewDemo/TableViewControllerPage.m
@@ -15,9 +15,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(20, 30, 100, 28)];
-    label1.text = @"this is new page";
+    UILabel *label1 = [[UILabel alloc] init];
+    NSString *desc = @"This method is a convenience constructor for creating button objects with specific configurations. If you subclass UIButton, this method does not return an instance of your subclass. If you want to create an instance of a specific subclass, you must alloc/init the button directly.";
+    label1.text = desc;
+    label1.font = [UIFont systemFontOfSize:14];
     label1.textColor = [UIColor redColor];
+    label1.backgroundColor = [UIColor yellowColor];
+    label1.numberOfLines = 0;
+    CGFloat textW = 150;
+    NSDictionary *textAtt = @{NSFontAttributeName: [UIFont systemFontOfSize:14]};
+    CGSize textSize = CGSizeMake(textW, MAXFLOAT);
+    CGFloat textH = [desc boundingRectWithSize:textSize options:NSStringDrawingUsesLineFragmentOrigin attributes:textAtt context:nil].size.height;
+    label1.frame = CGRectMake(10, 30, textW, textH);
     [self.view addSubview:label1];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
