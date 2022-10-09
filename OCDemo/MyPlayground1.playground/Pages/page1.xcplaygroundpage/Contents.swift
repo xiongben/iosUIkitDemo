60b1f047b1df61739a9ff5d74f0f3a16d3b5ebad
kakaxi <kakaxi@gmail.com>
2022-10-09 21:09:54 +0800

    [add]

---
+++ OCDemo/MyPlayground1.playground/Pages/page1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+
+let age:Int
+age = 10
+Int16.max
+Int8.min
---
+++ OCDemo/MyPlayground1.playground/Pages/page2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+import PlaygroundSupport
+
+var greeting = "Hello, playground"
+
+//let view = UIView()
+//view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//view.backgroundColor = UIColor.blue
+//PlaygroundPage.current.liveView = view
