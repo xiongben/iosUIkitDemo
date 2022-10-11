016b0b443042d81f1fc318613789ce0fecb1f90f
kakaxi <kakaxi@gmail.com>
2022-10-11 23:06:06 +0800

    [add]storage

--- OCDemo/MyPlayground1.playground/Pages/page2.xcplaygroundpage/Contents.swift
+++ OCDemo/MyPlayground1.playground/Pages/page2.xcplaygroundpage/Contents.swift
@@ -7,3 +7,16 @@ var greeting = "Hello, playground"
 //view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
 //view.backgroundColor = UIColor.blue
 //PlaygroundPage.current.liveView = view
+
+//结构体
+struct Point {
+    var x: Int = 0
+    var y: Int = 0
+    var origin: Bool = false
+}
+
+MemoryLayout<Point>.size
+MemoryLayout<Point>.stride
+
+var ptr = malloc(16)
+malloc_size(ptr)
