47725d87ecc913657203467951505965bed6c4a4
kakaxi <kakaxi@gmail.com>
2022-10-26 21:40:19 +0800

    [add]

--- OCDemo/TelephoneApp/TelephoneApp/ViewController.m
+++ OCDemo/TelephoneApp/TelephoneApp/ViewController.m
@@ -21,12 +21,20 @@
     UILabel *accountNum = [[UILabel alloc] initWithFrame: CGRectMake(30, 90, 50, 40)];
     accountNum.text = @"账号:";
     
-    UILabel *accountPwd = [[UILabel alloc] initWithFrame: CGRectMake(30, 130, 50, 40)];
+    UILabel *accountPwd = [[UILabel alloc] initWithFrame: CGRectMake(30, 150, 50, 40)];
     accountPwd.text = @"密码:";
+    
+    UITextField *accountNumInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 90, 120, 40)];
+    accountNumInput.backgroundColor = [UIColor grayColor];
+    
+    UITextField *accountPwdInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 150, 120, 40)];
+    accountPwdInput.backgroundColor = [UIColor grayColor];
     
     
     [self.view addSubview:accountNum];
     [self.view addSubview:accountPwd];
+    [self.view addSubview:accountNumInput];
+    [self.view addSubview:accountPwdInput];
 }
 
 
