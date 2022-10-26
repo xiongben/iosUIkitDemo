2ff6943242ca4d0c14323482a100d84a1d3d0fe3
kakaxi <kakaxi@gmail.com>
2022-10-26 22:14:42 +0800

    [add]

--- OCDemo/TelephoneApp/TelephoneApp/ViewController.m
+++ OCDemo/TelephoneApp/TelephoneApp/ViewController.m
@@ -24,12 +24,31 @@
     UILabel *accountPwd = [[UILabel alloc] initWithFrame: CGRectMake(30, 150, 50, 40)];
     accountPwd.text = @"密码:";
     
-    UITextField *accountNumInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 90, 120, 40)];
+    UITextField *accountNumInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 90, 220, 40)];
     accountNumInput.backgroundColor = [UIColor grayColor];
+    accountNumInput.clearButtonMode = UITextFieldViewModeWhileEditing;
     
-    UITextField *accountPwdInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 150, 120, 40)];
+    UITextField *accountPwdInput = [[UITextField alloc] initWithFrame:CGRectMake(80, 150, 220, 40)];
     accountPwdInput.backgroundColor = [UIColor grayColor];
+    accountPwdInput.clearButtonMode = UITextFieldViewModeWhileEditing;
     
+    UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(20, 220, 350, 80)];
+    [self.view addSubview:view1];
+    
+    UILabel *rememberPwd = [[UILabel alloc] initWithFrame: CGRectMake(20, 10, 80, 40)];
+    rememberPwd.text = @"记住密码";
+    
+    UISwitch *pwdSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(110, 10, 80, 40)];
+    
+    UILabel *autoLogin = [[UILabel alloc] initWithFrame: CGRectMake(200, 10, 80, 40)];
+    autoLogin.text = @"自动登录";
+    
+    UISwitch *loginSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(290, 10, 80, 40)];
+    [view1 addSubview:rememberPwd];
+    [view1 addSubview:autoLogin];
+    [view1 addSubview:pwdSwitch];
+    [view1 addSubview:loginSwitch];
+
     
     [self.view addSubview:accountNum];
     [self.view addSubview:accountPwd];
